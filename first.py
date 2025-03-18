b8d68ed05600a128638f658d685fbe0fd4404729
Donald Cameron <donald.cameron@qa.com>
2025-03-18 05:03:54 +0000

    added first.py

---
+++ first.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python3
+# Author: DCameron
+# Version: 1.0
+# Description: This is our first script
+
+name = "Donald Cameron"
+print("My name is", name)
+print("My name is " + name)
