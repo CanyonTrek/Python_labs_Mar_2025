236a1d66a4f18e0e4700ac33404fe574b7fffbab
Donald Cameron <donald.cameron@ntlworld.com>
2025-03-19 08:14:16 +0000

    Updated sep.py

--- labs/sep.py
+++ labs/sep.py
@@ -1,4 +1,12 @@
 #! /usr/bin/python
 
+#                    1         2         3         4         5
+#          012345678901234567890123456789012345678901234567890
 Belgium = 'Belgium,10445852,Brussels,737966,Europe,1830,Euro,Catholicism,Dutch,French,German'
 
+print("-" * len(Belgium)) # Q1
+print(Belgium.replace(',', ':')) # Q2
+pop = int(Belgium[8:16])
+pop_city = int(Belgium[26:32])
+print("Population=", pop + pop_city) # Q3
+print("-" * len(Belgium)) # Q4
