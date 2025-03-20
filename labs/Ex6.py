8d5c87d23454c37dd15e2d9875d95814d49351f8
Donald Cameron <donald.cameron@ntlworld.com>
2025-03-20 00:45:54 +0000

    Solved Ex6.py

--- labs/Ex6.py
+++ labs/Ex6.py
@@ -15,19 +15,18 @@ for postcode in infile:
     if postcode.isspace(): continue
     
     # TODO (a): Remove newlines, tabs and spaces.
-    
+    postcode = re.sub(r"\s+", r"", postcode)
     
     # TODO (a): Convert to uppercase.
-    
+    postcode = postcode.upper()
 
     # TODO (a): Insert a space before the final digit and 2 letters.
-    
-    
+    postcode = re.sub(r"(\d[A-Z]{2})$", r" \1", postcode)
     # Print the reformatted postcode.
-    print (postcode)
+    print(postcode)
 
     # TODO (b) Validate the postcode, returning a match object 'm'.
-    m = 0   # TODO (b) Replace this line with a call to re.match().
+    m = re.search(r"^[A-Z]{1,2}\d{1,2}[A-Z]?\s\d[A-Z]{2}$", postcode)
     
     if m:
         valid = valid + 1
@@ -38,4 +37,5 @@ for postcode in infile:
 infile.close()
 
 # TODO (b) Print the valid and invalid totals.
-
+print(f"Valid postcodes = {valid}")
+print(f"Invalid postcodes = {invalid}")
