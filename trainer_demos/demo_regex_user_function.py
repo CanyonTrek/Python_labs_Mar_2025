1eb0f81d6c4c96c203ac93ae93e10ef87b0a8a6c
Donald Cameron <donald.cameron@ntlworld.com>
2025-03-21 03:06:19 +0000

    Updated demo_regex

--- trainer_demos/demo_regex_user_function.py
+++ trainer_demos/demo_regex_user_function.py
@@ -6,6 +6,7 @@
     Docstring:
 """
 import re
+import sys
 
 # Example of a user function with optional parameter passing and
 # default values.
@@ -13,15 +14,24 @@ def search_pattern(pattern=r"^.{19}$", file=r"C:\Users\Donal\course\Python_labs_
     lines = 0
     # Open file handle for READING in TEXT mode.
     with open(file, mode="rt") as fh_in:
+        reobj = re.compile(pattern) # Precompile pattern ONCE
         for line in fh_in:
-           m = re.search(pattern, line)  # MATCH lines start/end SAME CAPITAL
+           # m = re.search(pattern, line)  # MATCH lines start/end SAME CAPITAL
+           m = reobj.search(line)
            if m:
                 lines += 1
                 print(f"Matched {m.group()} on {line.rstrip()} at {m.start()}-{m.end()}")
     return lines
 
-lines_matched = 0
-lines_matched += search_pattern()
-lines_matched += search_pattern(r"^([A-Z]).*\1$",
-                                file=r"C:\Users\Donal\course\Python_labs_Mar_2025\labs\words")
-print(f"Lines matched = {lines_matched}")
+def main():
+    lines_matched = 0
+    lines_matched += search_pattern()
+    print(f"Lines matched = {lines_matched}")
+    return None
+
+if __name__ == "__main__":
+    import cProfile
+    # cProfile.run("main()") # Analysis to the console
+    cProfile.run("main()", "stats.prof")  # Analysis to a file
+    # main()
+    sys.exit(0)
